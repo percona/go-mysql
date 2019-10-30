20d1c85af2aa6b8d6de9d48ab8d3f2b0793582b7
粟含 <suhan03@meituan.com>
2019-10-30 21:01:07 +0800

    add missing blank for test TestParseSlowLog002

--- log/slow/parser_test.go
+++ log/slow/parser_test.go
@@ -158,7 +158,7 @@ func TestParseSlowLog002(t *testing.T) {
 		{
 			Db: "db1",
 			Query: `update db2.tuningdetail_21_265507 n
-      inner join db1.gonzo a using(gonzo)
+      inner join db1.gonzo a using(gonzo) 
       set n.column1 = a.column1, n.word3 = a.word3`,
 			Admin:     false,
 			User:      "[SQL_SLAVE]",
